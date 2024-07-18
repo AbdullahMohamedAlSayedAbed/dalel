fa5b0faa63eec3bc2e354f6bcf470a6612bc90ab
Abdullah Abed <abedabdallah834@gmail.com>
2024-07-18 12:12:36 +0300

    create App images and add all images

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,40 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesForgotPassword
+  /// assets/images/forgotPassword.png
+  static const String imagesForgotPassword = "assets/images/forgotPassword.png";
+
+  /// Assets for imagesOnBoarding1
+  /// assets/images/onBoarding1.png
+  static const String imagesOnBoarding1 = "assets/images/onBoarding1.png";
+
+  /// Assets for imagesOnBoarding2
+  /// assets/images/onBoarding2.png
+  static const String imagesOnBoarding2 = "assets/images/onBoarding2.png";
+
+  /// Assets for imagesOnBoarding3
+  /// assets/images/onBoarding3.png
+  static const String imagesOnBoarding3 = "assets/images/onBoarding3.png";
+
+  /// Assets for imagesSalip
+  /// assets/images/salip.svg
+  static const String imagesSalip = "assets/images/salip.svg";
+
+  /// Assets for imagesSucccesOrderIcon
+  /// assets/images/SucccesOrderIcon.png
+  static const String imagesSucccesOrderIcon = "assets/images/SucccesOrderIcon.png";
+
+  /// Assets for imagesUnsplashMoonoldXeqs
+  /// assets/images/unsplash_MoonoldXeqs.png
+  static const String imagesUnsplashMoonoldXeqs = "assets/images/unsplash_MoonoldXeqs.png";
+
+  /// Assets for imagesVector1
+  /// assets/images/Vector1.svg
+  static const String imagesVector1 = "assets/images/Vector1.svg";
+
+  /// Assets for imagesVector2
+  /// assets/images/Vector2.svg
+  static const String imagesVector2 = "assets/images/Vector2.svg";
+}
+
